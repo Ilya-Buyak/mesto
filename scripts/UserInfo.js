7df99038646121213cfb1efb2e6eba5dd668d4a2
Ilya <Ilya_11_11@mail.ru>
2020-06-02 20:10:41 +0300

    mirror fix

--- scripts/UserInfo.js
+++ scripts/UserInfo.js
@@ -3,13 +3,12 @@ class UserInfo{
     this.name = obj.name
     this.job = obj.job
     this.api = obj.api
-    this.formName = obj.editUserForm.name
-    this.formJob = obj.editUserForm.about
+    this.formElement = obj.editUserForm
     this.btn = obj.editUserForm.querySelector('button')
   }
 
   setUserInfo () {
-    this.api.editUser({ name: this.formName.value, job: this.formJob.value })
+    this.api.editUser({ name: this.formElement.name.value, job: this.formElement.about.value })
       .then((res) => {
         this.updateUserInfo(res.name, res.about);
       })
