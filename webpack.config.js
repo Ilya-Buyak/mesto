cd6d3646ea023447ba5a6ef11c0e4a7953bb02c4
Ilya <Ilya_11_11@mail.ru>
2020-07-10 21:38:14 +0300

    fix autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,12 @@ module.exports = {
     },
       {
         test: /\.css$/i,
-        use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, {
+          loader: "css-loader",
+          options: {
+            importLoaders: 2
+          }
+        }, 'postcss-loader'],
       },
       {
         test: /\.(png|jpg|gif|ico|svg)$/,
